072e6ea46731fe6223415a739086b571cdd3c6f4
eduardomarcon <eduardommarcon@gmail.com>
2025-01-10 11:18:40 -0300

    test: adding task integration test

---
+++ api/src/test/java/com/showcase/api/controller/TaskControllerITTest.java
@@ -0,0 +1,48 @@
+package com.showcase.api.controller;
+
+import com.showcase.api.controller.data.request.TaskCreateRequest;
+import com.showcase.api.domain.model.Task;
+import com.showcase.api.domain.service.CreateTaskService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class TaskControllerITTest extends ControllerCommon {
+
+	@Autowired
+	private CreateTaskService createTaskService;
+
+	@Test
+	void shouldInsertATask() throws Exception {
+		var taskCreateRequest = new TaskCreateRequest("title", "description");
+
+		mockMvc
+				.perform(post("/tasks")
+								 .contentType(MediaType.APPLICATION_JSON)
+								 .content(asJsonString(taskCreateRequest)))
+				.andExpect(status().isCreated());
+	}
+
+	@Test
+	void shouldUpdateTheCompletedFieldOfTheTask() throws Exception {
+		var createdTask = insertANewTask();
+
+		mockMvc
+				.perform(patch("/tasks/" + createdTask.getId() + "/completed"))
+				.andExpect(status().isOk());
+	}
+
+	private Task insertANewTask() {
+		var newTask = new Task();
+		newTask.setTitle("title");
+		newTask.setDescription("description");
+		newTask.setCompleted(true);
+
+		return createTaskService.execute(newTask);
+	}
+
+}
