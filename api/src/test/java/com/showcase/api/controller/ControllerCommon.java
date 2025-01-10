32c34a077579e0990c23e097c691f8033d7f95b4
eduardomarcon <eduardommarcon@gmail.com>
2025-01-10 11:17:43 -0300

    test: configuring common integration test

--- api/src/test/java/com/showcase/api/controller/ControllerCommon.java
+++ api/src/test/java/com/showcase/api/controller/ControllerCommon.java
@@ -2,15 +2,28 @@ package com.showcase.api.controller;
 
 import com.showcase.api.config.TodoPostgresqlContainer;
 import org.junit.ClassRule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 @Testcontainers
 public class ControllerCommon {
+
+	@Autowired
+	protected MockMvc mockMvc;
 
 	@ClassRule
 	public static PostgreSQLContainer<TodoPostgresqlContainer> pgSQLContainer = TodoPostgresqlContainer.getInstance();
 
+	protected String asJsonString(Object object) throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.writeValueAsString(object);
+	}
 }
