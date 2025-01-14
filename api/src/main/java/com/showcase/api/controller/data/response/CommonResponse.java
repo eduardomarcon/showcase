124b3769158621888209c47c24a4b0c79a9aae06
eduardomarcon <eduardommarcon@gmail.com>
2025-01-14 09:26:27 -0300

    feat: adding constructor for default timestamp and null message

--- api/src/main/java/com/showcase/api/controller/data/response/CommonResponse.java
+++ api/src/main/java/com/showcase/api/controller/data/response/CommonResponse.java
@@ -2,4 +2,10 @@ package com.showcase.api.controller.data.response;
 
 import java.time.LocalDateTime;
 
-public record CommonResponse<T>(T data, String message, LocalDateTime timestamp) {}
+public record CommonResponse<T>(T data, String message, LocalDateTime timestamp) {
+
+	public CommonResponse(T data) {
+		this(data, null, LocalDateTime.now());
+	}
+
+}
