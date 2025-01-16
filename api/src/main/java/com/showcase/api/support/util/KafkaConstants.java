c89a9a7b62a3b933a97262b6f2e96a05a875cdfa
eduardomarcon <eduardommarcon@gmail.com>
2025-01-16 19:38:43 -0300

    feat: adding and configuring kafka integration to the project

---
+++ api/src/main/java/com/showcase/api/support/util/KafkaConstants.java
@@ -0,0 +1,13 @@
+package com.showcase.api.support.util;
+
+public class KafkaConstants {
+
+	private KafkaConstants() {}
+
+	public static final class Topics {
+		private Topics() {}
+
+		public static final String TASK_COMPLETED = "task-completed";
+	}
+
+}
