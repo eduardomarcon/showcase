ee4f91258b1c15f32203577ae6b2fa5f4c2fe43a
eduardomarcon <eduardommarcon@gmail.com>
2025-01-16 20:33:05 -0300

    refactor: moving configuration for date-time formatting to a specific file

--- api/src/main/java/com/showcase/api/config/RedisConfig.java
+++ api/src/main/java/com/showcase/api/config/RedisConfig.java
@@ -1,30 +1,23 @@
 package com.showcase.api.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.text.SimpleDateFormat;
-
 @Configuration
 @EnableCaching
 public class RedisConfig {
 
-	@Bean
-	@Primary
-	public ObjectMapper objectMapper(){
-		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.registerModule(new JavaTimeModule());
-		objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"));
-		return objectMapper;
+	private final ObjectMapper objectMapper;
+
+	public RedisConfig(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
 	}
 
 	@Bean
@@ -37,9 +30,9 @@ public class RedisConfig {
 		RedisTemplate<String, String> template = new RedisTemplate<>();
 		template.setConnectionFactory(connectionFactory);
 		template.setKeySerializer(new StringRedisSerializer());
-		template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()));
+		template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
 		template.setHashKeySerializer(new StringRedisSerializer());
-		template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()));
+		template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
 		template.afterPropertiesSet();
 
 		return template;
