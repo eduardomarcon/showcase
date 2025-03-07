1932fe936757aec2e0f8679bbce391c767fc2832
eduardomarcon <eduardommarcon@gmail.com>
2025-03-07 08:50:25 -0300

    chore: simplifying redis config

--- api/src/main/java/com/showcase/api/config/CustomJacksonConfig.java
+++ api/src/main/java/com/showcase/api/config/CustomJacksonConfig.java
@@ -3,38 +3,24 @@ package com.showcase.api.config;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class CustomJacksonConfig {
 
-	private static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+	private static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
 
 	@Bean
 	public ObjectMapper objectMapper() {
 		ObjectMapper mapper = new ObjectMapper();
-
-		SimpleModule module = new SimpleModule();
-		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(DATETIME_FORMAT);
-
-		module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(dateTimeFormatter));
-		module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(dateTimeFormatter));
-
-		mapper.registerModule(module);
 		mapper.registerModule(new JavaTimeModule());
-		mapper.setDateFormat(new SimpleDateFormat(DATETIME_FORMAT));
-
+		mapper.setDateFormat(new SimpleDateFormat(DATE_TIME_FORMAT));
 		mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-		mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
 		return mapper;
 	}
--- api/src/main/java/com/showcase/api/config/RedisConfig.java
+++ api/src/main/java/com/showcase/api/config/RedisConfig.java
@@ -5,7 +5,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -21,19 +20,13 @@ public class RedisConfig {
 	}
 
 	@Bean
-	public LettuceConnectionFactory connectionFactory() {
-		return new LettuceConnectionFactory();
-	}
-
-	@Bean
-	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-		RedisTemplate<String, String> template = new RedisTemplate<>();
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
 		template.setConnectionFactory(connectionFactory);
 		template.setKeySerializer(new StringRedisSerializer());
-		template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
-		template.setHashKeySerializer(new StringRedisSerializer());
-		template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
-		template.afterPropertiesSet();
+		GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+		template.setValueSerializer(serializer);
+		template.setHashValueSerializer(serializer);
 
 		return template;
 	}
